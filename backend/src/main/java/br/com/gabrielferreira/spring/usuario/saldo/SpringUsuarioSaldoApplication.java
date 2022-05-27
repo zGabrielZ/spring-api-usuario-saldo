d1a39cfd7636677464ca3a8d31e403380c8eb6a3
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2022-05-27 17:27:59 -0300

    Projeto criado

---
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/SpringUsuarioSaldoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.spring.usuario.saldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsuarioSaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsuarioSaldoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gabrielferreira/spring/usuario/saldo/SpringUsuarioSaldoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.spring.usuario.saldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsuarioSaldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
