2f7353963aab43f8caef751d7d9024a57a2803fd
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2022-05-29 14:30:54 -0300

    Implementação ao banco de dados teste H2 e mapemanento nas classes jpa

--- backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/entidade/Saldo.java
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/entidade/Saldo.java
@@ -2,10 +2,13 @@ package br.com.gabrielferreira.spring.usuario.saldo.entidade;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "SALDO")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,11 +19,19 @@ public class Saldo implements Serializable {
 
     private static final long serialVersionUID = -9124903408974073315L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(name = "deposito",nullable = false)
     private BigDecimal deposito;
+
+    @Column(name = "data_deposito", nullable = false)
     private LocalDateTime dataDeposito;
+
+    @JoinColumn(name = "usuario_id",foreignKey = @ForeignKey(name="usuario_fk"))
+    @ManyToOne
     private Usuario usuario;
 
 }
--- backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/entidade/Usuario.java
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/entidade/Usuario.java
@@ -2,11 +2,14 @@ package br.com.gabrielferreira.spring.usuario.saldo.entidade;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "USUARIO")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,13 +19,33 @@ public class Usuario implements Serializable {
 
     private static final long serialVersionUID = -7061711027863603940L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(name = "nome",nullable = false)
     private String nome;
+
+    @Column(name = "email",nullable = false)
     private String email;
+    @Column(name = "senha",nullable = false)
     private String senha;
+
+    @Column(name = "cpf",nullable = false)
     private String cpf;
+    @Column(name = "data_nascimento",nullable = false)
     private LocalDate dataNascimento;
+
+    @OneToMany(cascade = CascadeType.REMOVE,fetch = FetchType.EAGER,mappedBy = "usuario")
     private List<Saldo> saldos = new ArrayList<>();
+
+    public Usuario(Long id, String nome, String email, String senha, String cpf, LocalDate dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
 }
