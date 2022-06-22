975464b2d97820ddc569e9bf2fe3141d86f91dd7
gabrielferreira <gabrielferreira@magnasistemas.com.br>
2022-06-22 15:29:23 -0300

    Ajustes finais do projeto

--- backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/entidade/dto/SaqueViewDTO.java
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/entidade/dto/SaqueViewDTO.java
@@ -2,6 +2,7 @@ package br.com.gabrielferreira.spring.usuario.saldo.entidade.dto;
 
 import br.com.gabrielferreira.spring.usuario.saldo.entidade.Saque;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.springframework.data.domain.Page;
 
@@ -15,9 +16,11 @@ public class SaqueViewDTO implements Serializable {
     private static final long serialVersionUID = -7136821550686871414L;
 
 
+    @ApiModelProperty(value = "Data do saque", example = "26/06/2022 12:00:00")
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime dataSaque;
 
+    @ApiModelProperty(value = "Valor saque do usuário", example = "500.00")
     private BigDecimal valor;
 
     public SaqueViewDTO(Saque saque){
