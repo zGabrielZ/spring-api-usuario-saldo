0b6678b8e5ad44468163daf0a91ce0c726072fcd
gabrielferreira <gabrielferreira@magnasistemas.com.br>
2022-11-07 19:47:52 -0300

    Ajuste post usuario

--- backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/dominio/dto/pefil/PerfilInsertFormDTO.java
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/dominio/dto/pefil/PerfilInsertFormDTO.java
@@ -1,5 +1,6 @@
 package br.com.gabrielferreira.spring.usuario.saldo.dominio.dto.pefil;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +18,6 @@ public class PerfilInsertFormDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = -1708090859850629848L;
 
+    @ApiModelProperty(value = "ID do perfil", example = "1")
     private Long id;
 }
