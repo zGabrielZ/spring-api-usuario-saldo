4b1edbf0408636e39da6a72b5a0bde63efabc35d
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-02-13 20:56:26 -0300

    Aplicando modelo swagger autenticação

--- backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/controller/AutenticacaoController.java
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/controller/AutenticacaoController.java
@@ -4,15 +4,16 @@ import br.com.gabrielferreira.spring.usuario.saldo.dominio.dto.autenticacao.Aute
 import br.com.gabrielferreira.spring.usuario.saldo.dominio.dto.autenticacao.TokenDTO;
 import br.com.gabrielferreira.spring.usuario.saldo.dominio.dto.factory.AutenticacaoDTOFactory;
 import br.com.gabrielferreira.spring.usuario.saldo.service.security.TokenService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,6 +26,12 @@ public class AutenticacaoController {
 
     private final TokenService tokenService;
 
+    @ApiOperation("Autenticou um usuário")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201,message = "Autenticou um usuário"),
+            @ApiResponse(code = 400,message = "Ocorreu um erro personalizado"),
+    })
     @PostMapping
     public ResponseEntity<TokenDTO> autenticar(@Valid @RequestBody AutenticacaoFormDTO autenticacaoFormDTO){
         UsernamePasswordAuthenticationToken dadosLogin = AutenticacaoDTOFactory.toUsernamePassword(autenticacaoFormDTO);
--- backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/dominio/dto/autenticacao/AutenticacaoFormDTO.java
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/dominio/dto/autenticacao/AutenticacaoFormDTO.java
@@ -1,5 +1,6 @@
 package br.com.gabrielferreira.spring.usuario.saldo.dominio.dto.autenticacao;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,10 +20,12 @@ public class AutenticacaoFormDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = -2132474368541690403L;
 
+    @ApiModelProperty(value = "E-mail do usuário", example = "test@email.com.br")
     @NotBlank(message = "E-mail não pode ser vazio.")
     @Email(message = "E-mail inválido.")
     private String email;
 
+    @ApiModelProperty(value = "Senha do usuário", example = "123")
     @NotBlank(message = "Senha não pode ser vazio.")
     private String senha;
 }
