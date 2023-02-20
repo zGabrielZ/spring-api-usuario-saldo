7914c6e839ea79e8fb6bd7ba08567b0fa7ab31a3
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-02-20 20:26:45 -0300

    Correção consumir endpoint por fora

--- backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/dominio/dto/FeriadoNacionalDTO.java
+++ backend/src/main/java/br/com/gabrielferreira/spring/usuario/saldo/dominio/dto/FeriadoNacionalDTO.java
@@ -1,5 +1,6 @@
 package br.com.gabrielferreira.spring.usuario.saldo.dominio.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class FeriadoNacionalDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = -6871728252703607321L;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
     private String name;
 }
